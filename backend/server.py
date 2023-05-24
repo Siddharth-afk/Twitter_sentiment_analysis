863db16bf11a1890f36391588848513bc11700b3
h <h@gmail.com>
2023-05-24 19:07:26 +0530

    worked on the frontend

--- backend/server.py
+++ backend/server.py
@@ -8,19 +8,15 @@ import emoji
 import snscrape.modules.twitter as sntwitter
 
 app = Flask(__name__)
- 
-""" #members api routes
-
-@app.route("/members")
-def members():
-    return{"members": []} """
+save_directory = "C:/Users/gupta/Documents/finalYearProject(final)/projectfinal/src/images/"
+plt.switch_backend('agg')
 
 @app.route("/twitter", methods=['POST', 'GET'])
 def test():
     posts = []
     data = request.json['data']
     for i, tweets in enumerate(sntwitter.TwitterSearchScraper(f"{data}").get_items()):
-        if i > 10:
+        if i > 100:
             break
         posts.append(tweets.content)
 
@@ -32,6 +28,8 @@ def test():
         text = re.sub(r'#', '', text)
         text = re.sub(r'RT[\s]+', '', text)
         text = re.sub(r'https?:\/\/\S+', '', text)
+        text = re.sub(r'_', ' ', text)
+        text = re.sub(r'&amp;', 'and', text)
 
         return text
     
@@ -59,24 +57,44 @@ def test():
     
     #bar graph plot
     df['analysis'].value_counts()
-    plt.title("sentiment analysis")
+    plt.title("Bargraph")
     plt.xlabel("sentiment")
     plt.ylabel("counts")
     df['analysis'].value_counts().plot(kind = 'bar')
-    plt.savefig('bargraph', bbox_inches='tight', pad_inches=0.3, transparent=False)
+    plt.savefig(save_directory + 'bargraph')
+    df['bargraph'] = save_directory + 'bargraph'
 
     #scatterplot of sentiment vs polarity
     plt.figure(figsize = (8, 6))
     for i in range(0, df.shape[0]):
         plt.scatter(df['Polarity'][i], df['Subjectivity'][i], color = "blue")
 
-    plt.title("scatterplot")
-    plt.xlabel("polarity")
-    plt.ylabel("subjectivity")   
-    plt.savefig('scatterplot')
+
+    plt.title("Scatterplot")
+    plt.xlabel("Polarity")
+    plt.ylabel("Subjectivity")   
+    plt.savefig(save_directory + 'scatterplot')
+    df['scatterplot'] = save_directory + 'bargraph'
+
+    ptweets = df[df.analysis == "positive"]
+    ptweets = ptweets['tweets']
+    perpos = round(len(ptweets)/df.shape[0] * 100, 1)
+
+    df['perpos'] = perpos
+
+    ntweets = df[df.analysis == "negative"]
+    ntweets = ntweets['tweets']
+    perneg = round(len(ntweets)/len(df) * 100, 1)
+
+    df['perneg'] = perneg
+
+    nettweets = df[df.analysis == "neutral"]
+    nettweets = nettweets['tweets']
+    pernet = round(len(nettweets)/len(df) * 100, 1)
+
+    df['pernet'] = pernet
 
     return(df.to_json())
-
 
 if __name__ == "__main__":
     app.run(debug=True)
